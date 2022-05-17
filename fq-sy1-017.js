de8cda77553ed18d5fd81e7a9509deb2ff230d71
doujiang2019 <93854029+doujiang2019@users.noreply.github.com>
2022-05-17 13:32:57 +0800

    Upload by PicGo

---
+++ fq-sy1-017.js
@@ -0,0 +1,56 @@
+﻿<div class="text-sy1">
+<table class="ggtables" border="1" width="100%" cellpadding="0" cellspacing="0" bordercolorlight="#FFFFFF" bordercolordark="#FFFFFF" bgcolor="#FFFFFF" bordercolor="#e5e5e5"><tbody><tr><td class="center f13 black l150" bgcolor="#800000">
+	<p align="center">
+	<img src="https://cdn.jsdelivr.net/gh/doujiang2019/new@main/lwdj4o (1).jpg"></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://065858.com/#w1" style="TEXT-DECORATION: none" onfocus="undefined">
+		<font color="#000000" face="微软雅黑"><font style="font-weight: bold;" color="#333333">澳门彩</font><b style="">:<font color="#0066ff">全网最牛逼的三肖三码一直连中</font></b></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://524848.com/#w1" style="TEXT-DECORATION: none" onfocus="undefined">
+		<font color="#000000" face="微软雅黑"><font style="font-weight: bold;" color="#333333">澳门彩</font><b style="">:<font color="#ff0000">大神一码</font><font color="#ff00ff">带彩民朋友们发家致富</font></b></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://457272.com/#asy1" style="TEXT-DECORATION: none" onfocus="undefined">
+		<font color="#000000" face="微软雅黑"><font style="font-weight: bold;" color="#333333">澳彩</font><b style="">:</b><font color="#ff3300"><b>感恩三中三,650倍大回馈</b></font></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://964848.com/#w1" style="TEXT-DECORATION: none" onfocus="undefined"><font color="#000000" face="微软雅黑"><b>澳门彩:<span style="color: rgb(255, 0, 0); background-color: rgb(255, 255, 0);">赚钱一码</span><span style="background-color: rgb(255, 255, 255);"><font color="#0000ff">带领大家上岸发财</font></span></b></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="https://152323.com/#w1" style="TEXT-DECORATION: none" onfocus="undefined"><font color="#000000" face="微软雅黑"><font face="微软雅黑" style="" color="#ff0000"><b style="background-color: rgb(255, 255, 0);">澳门彩:</b></font><b style="background-color: rgb(255, 0, 255);"><font color="#ffff00">史上最稳六肖计划</font>30中25</b></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://127272.com/#asy1" style="TEXT-DECORATION: none" onfocus="undefined">
+		<font face="微软雅黑" style="background-color: rgb(255, 0, 0);" color="#ffffff"><b>澳门彩:</b></font><font color="#000000" face="微软雅黑"><span style="background-color: rgb(255, 255, 0);"><font color="#ff0000" style=""><b>澳门资讯一组四连肖</b></font></span></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://662121.com/#w1    " style="TEXT-DECORATION: none" onfocus="undefined">
+		<font color="#000000" face="微软雅黑"><b><font face="微软雅黑">澳门彩:<font color="#0000ff">顶流一肖一码</font><font color="#00cc66">带各位吃香的喝辣的</font></font></b></font></a></td></tr><tr><td class="center f13 black l150" height="33">
+		<p align="center"><a target="_blank" href="http://477878.com/#w1" style="TEXT-DECORATION: none" onfocus="undefined">
+		<font color="#000000" face="微软雅黑"><b><font face="微软雅黑">澳门彩:<font color="#0000ff">搜罗最强一肖一码</font><font color="#ff0000">期期公开</font></font></b></font></a></td></tr></tbody></table>
+		
+		</div>
+		
+		<style>
+		
+		.text-sy1{
+  font-size:17pt;
+  font-weight:700;
+  text-transform:uppercase;
+  text-align:center;
+  margin:0 auto;
+  display:block;
+  position:relative}
+@keyframes animate{
+  0%{
+    opacity:.1}
+  100%{
+    opacity:1;
+    text-shadow:0 0 60px Red,0 0 15px orange,0 0 6px DarkRed}
+}
+@media only screen and (max-width:990px){
+  .text-sy1{
+    font-size:24px}
+}
+@media only screen and (max-width:767px){
+  .text-sy1{
+    font-size:24px}
+}
+@media only screen and (max-width:479px){
+  .text-sy1{
+    font-size:18px}
+}
+@media only screen and (max-width:359px){
+  .text-sy1{
+    font-size:16px}
+}
+		
+		</style>
